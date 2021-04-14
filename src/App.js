83c8dbceec91678694a42528dedf749c7dc9acd9
Anwar <AnwarAldogmi@gmail.com>
2021-04-13 19:13:52 -0700

    completed all tasks except stretch searchbar

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const App = () => {
       {/* //* Add SearchBar and Posts here to render them */}
       {<SearchBar /> } 
       {/* /* Check the implementation of each component, to see what props they require, if any! */}
-      {<post likePost={likePost} posts={posts} />}
+      {<Posts likePost={likePost} posts={posts} />}
     </div>
   );
 };
